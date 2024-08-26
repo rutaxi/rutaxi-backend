bc4a22629e70a016c089887254dd0e352e71af62
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-08-26 19:37:45 +0900

    [Feat/#25] redirect page 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router';
-import { useLocation } from 'react-router-dom';
-import { CreateTaxiParty, EditProfile, Main, MyPage, TaxiPartyChat, TaxiPartyDetail, TaxiPartyList } from './pages';
-import NavBar from './components/navBar/NavBar';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router'
+import { useLocation } from 'react-router-dom'
+import {
+  CreateTaxiParty,
+  EditProfile,
+  Main,
+  MyPage,
+  TaxiPartyChat,
+  TaxiPartyDetail,
+  TaxiPartyList,
+} from './pages'
+import NavBar from './components/navBar/NavBar'
+import './App.css'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token') ? true : false);
-  const [userName, setUserName] = useState(localStorage.getItem('userName') ? localStorage.getItem('userName') : '');
-  const [userEmail, setUserEmail] = useState(localStorage.getItem('userEmail') ? localStorage.getItem('userEmail') : '');
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem('token') ? true : false
+  )
+  const [userName, setUserName] = useState(
+    localStorage.getItem('userName') ? localStorage.getItem('userName') : ''
+  )
+  const [userEmail, setUserEmail] = useState(
+    localStorage.getItem('userEmail') ? localStorage.getItem('userEmail') : ''
+  )
 
-  const location = useLocation();
-  const disappearNavBar = !(location.pathname === '/taxi-party-chat' || location.pathname === '/edit-profile');
+  const location = useLocation()
+  const disappearNavBar = !(
+    location.pathname === '/taxi-party-chat' ||
+    location.pathname === '/edit-profile'
+  )
 
-  const [navActive, setNavActive] = useState(location.pathname === '/taxi-party-list' ? 'list' : location.pathname === '/mypage' ? 'search' : 'home');
-  const [navChage, setNavChange] = useState(false);
+  const [navActive, setNavActive] = useState(
+    location.pathname === '/taxi-party-list'
+      ? 'list'
+      : location.pathname === '/mypage'
+        ? 'search'
+        : 'home'
+  )
+  const [navChage, setNavChange] = useState(false)
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('userName');
-    localStorage.removeItem('userEmail');
-    document.cookie = 'rutaxiRefreshToken=; Max-Age=0; path=/;';
+    localStorage.removeItem('token')
+    localStorage.removeItem('userName')
+    localStorage.removeItem('userEmail')
+    document.cookie = 'rutaxiRefreshToken=; Max-Age=0; path=/;'
 
-    setIsLoggedIn(false);
+    setIsLoggedIn(false)
     // closeModal();
     // goToMain();
   }
@@ -30,17 +53,56 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUserName={setUserName} setUserEmail={setUserEmail} />} />
-        <Route path='/taxi-party-list' element={<TaxiPartyList />} />
-        <Route path='/taxi-party-detail' element={<TaxiPartyDetail userName={userName} />} />
-        <Route path='/create-taxi-party' element={<CreateTaxiParty />} />
-        <Route path='/taxi-party-chat' element={<TaxiPartyChat userName={userName} userEmail={userEmail} />} />
-        <Route path='/mypage' element={<MyPage setIsLoggedIn={setIsLoggedIn} userName={userName} handleLogout={handleLogout} />} />
-        <Route path='/edit-profile' element={<EditProfile setCurUserName={setUserName} />} />
+        <Route
+          path="/"
+          element={
+            <Main
+              isLoggedIn={isLoggedIn}
+              setIsLoggedIn={setIsLoggedIn}
+              setUserName={setUserName}
+              setUserEmail={setUserEmail}
+            />
+          }
+        />
+        <Route path="/taxi-party-list" element={<TaxiPartyList />} />
+        <Route
+          path="/taxi-party-detail"
+          element={<TaxiPartyDetail userName={userName} />}
+        />
+        <Route path="/create-taxi-party" element={<CreateTaxiParty />} />
+        <Route
+          path="/taxi-party-chat"
+          element={<TaxiPartyChat userName={userName} userEmail={userEmail} />}
+        />
+        <Route
+          path="/mypage"
+          element={
+            <MyPage
+              setIsLoggedIn={setIsLoggedIn}
+              userName={userName}
+              handleLogout={handleLogout}
+            />
+          }
+        />
+        <Route
+          path="/edit-profile"
+          element={<EditProfile setCurUserName={setUserName} />}
+        />
+        <Route
+          path="/google-redirect"
+          element={<GoogleRedirect setIsLoggedIn={setIsLoggedIn} />}
+        />
       </Routes>
-      {isLoggedIn && disappearNavBar && <NavBar navActive={navActive} setNavActive={setNavActive} navChage={navChage} setNavChange={setNavChange} />}
+      {isLoggedIn && disappearNavBar && (
+        <NavBar
+          navActive={navActive}
+          setNavActive={setNavActive}
+          navChage={navChage}
+          setNavChange={setNavChange}
+        />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
